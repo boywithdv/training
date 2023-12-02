70ea56570c6aa7bfc82a4850dc457fd9757c96f8
jukiya <jukiya8891@gmail.com>
2023-12-03 02:42:14 +0900

    commit

--- lib/components/background.dart
+++ lib/components/background.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:training/Test/test.dart';
-import 'package:training/components/Circle.dart';
 import 'package:training/components/backgroundAnimation.dart';
-import 'package:training/components/backgroundGradient.dart';
 
 class BackGround extends StatefulWidget {
   const BackGround({super.key});
