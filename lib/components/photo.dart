dea7e7d61211b436262f3f8be0e3e80edc3bece4
jukiya <jukiya8891@gmail.com>
2024-02-04 21:22:35 +0900

    画像をアップロードできるように修正

--- lib/components/photo.dart
+++ lib/components/photo.dart
@@ -1,5 +1,4 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:training/controller/picker_image.dart';
@@ -14,33 +13,64 @@ class Photo extends StatefulWidget {
 
 class _PhotoState extends State<Photo> {
   Uint8List? _image;
+  final _photoController = PhotoController();
+
   void selectImage() async {
-    Uint8List img = await pickImage(ImageSource.gallery);
-    setState(() {
-      _image = img;
-    });
+    Uint8List? img = await _photoController.pickImage(ImageSource.gallery);
+    if (img != null) {
+      await _photoController.uploadImageToFirebase(img);
+      setState(() {
+        _image = img;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: _image != null
-          ? CircleAvatar(
-              backgroundColor: Colors.transparent,
-              radius: widget.width,
-              backgroundImage: MemoryImage(_image!),
-            )
-          : CircleAvatar(
-              backgroundColor: Colors.transparent,
-              radius: 120,
-              child: ClipOval(
-                child: Image.asset(
-                  "assets/img/user1.png",
-                  width: 120,
-                  height: 120,
-                  fit: BoxFit.cover,
+          ? GestureDetector(
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                radius: 120,
+                child: ClipOval(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    width: 120,
+                    height: 120,
+                    child: Image.memory(
+                      _image!,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
               ),
+              onTap: () {
+                print(_image);
+                selectImage();
+              },
+            )
+          : GestureDetector(
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                radius: 120,
+                child: ClipOval(
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    child: Image.asset(
+                      "assets/img/user1.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              onTap: () {
+                print("hello world");
+                selectImage();
+              },
             ),
     );
   }
--- lib/controller/picker_image.dart
+++ lib/controller/picker_image.dart
@@ -1,10 +1,35 @@
-import 'package:image_picker/image_picker.dart';
+import 'dart:typed_data';
 
-pickImage(ImageSource source) async {
-  final ImagePicker _imagePicker = ImagePicker();
-  XFile? _file = await _imagePicker.pickImage(source: source);
-  if (_file != null) {
-    return await _file.readAsBytes();
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class PhotoController {
+  Future<void> uploadImageToFirebase(Uint8List imageBytes) async {
+    try {
+      //アップロード作業
+      String fileName =
+          DateTime.now().millisecondsSinceEpoch.toString(); // ファイル名をユニークにする
+      Reference storageReference =
+          FirebaseStorage.instance.ref().child('images/$fileName.png');
+      UploadTask uploadTask = storageReference.putData(imageBytes);
+      //
+      TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+      String downloadURL = await taskSnapshot.ref.getDownloadURL();
+      print("downLoad URL ; " + downloadURL);
+    } catch (e) {
+      print("Error uploading file: $e");
+    }
   }
-  print("No Images Selected");
+
+  Future<Uint8List?> pickImage(ImageSource source) async {
+    final ImagePicker _imagePicker = ImagePicker();
+    XFile? _file = await _imagePicker.pickImage(source: source);
+
+    if (_file != null) {
+      return await _file.readAsBytes();
+    }
+
+    print("No Images Selected");
+    return null;
+  }
 }
