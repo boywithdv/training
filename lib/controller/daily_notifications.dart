dc601debf720f1ffcddcd780548d737fcef052ef
jukiya <jukiya8891@gmail.com>
2024-02-09 13:00:29 +0900

    commit

--- lib/controller/daily_notifications.dart
+++ lib/controller/daily_notifications.dart
@@ -42,7 +42,7 @@ class ScheduleDailly8AMNofitications {
   tz.TZDateTime _nextInstanceOf8AM() {
     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
     tz.TZDateTime scheduledDate =
-        tz.TZDateTime(tz.local, now.year, now.month, now.day, 13);
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, 8);
     if (scheduledDate.isBefore(now)) {
       scheduledDate = scheduledDate.add(const Duration(days: 1));
     }
